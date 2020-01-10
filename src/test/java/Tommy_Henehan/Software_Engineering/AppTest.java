77c4b2b85308f2a7c90dbffaade27a4dab484687
TOMMY  HENEHAN <16363826@IT.NUIGALWAY.IE>
2020-01-10 15:24:23 +0000

    fix

--- src/test/java/Tommy_Henehan/Software_Engineering/AppTest.java
+++ src/test/java/Tommy_Henehan/Software_Engineering/AppTest.java
@@ -1,8 +1,7 @@
 package Tommy_Henehan.Software_Engineering;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class AppTest {
 
